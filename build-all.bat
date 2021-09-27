19c2576836e2b28a9a14c6b3509d3865503275cd
andreasGBL <29144928+andreasGBL@users.noreply.github.com>
2021-09-27 16:29:44 +0200

    Build script that builds multiple versions of the executable

---
+++ build-all.bat
@@ -0,0 +1,16 @@
+mkdir build
+cd build
+
+cmake -DPROJECT_NAME="VideoLow_Nvidia" -DHARDWARE_ACCELERATION_DEFAULT=1 -DWITH_CONSOLE=0 ..
+cmake --build . --config Release
+
+cmake -DPROJECT_NAME="VideoLow_AMD" -DHARDWARE_ACCELERATION_DEFAULT=2 -DWITH_CONSOLE=0 ..
+cmake --build . --config Release
+
+cmake -DPROJECT_NAME="VideoLow_DEBUG" -DHARDWARE_ACCELERATION_DEFAULT=0 -DWITH_CONSOLE=1 ..
+cmake --build . --config Release
+
+cmake -DPROJECT_NAME="VideoLow" -DHARDWARE_ACCELERATION_DEFAULT=0 -DWITH_CONSOLE=0 ..
+cmake --build . --config Release
+
+Pause
